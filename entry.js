c9582793b664ae4e164735d55936a28c70e470fc
Sebastian Cruz <s.sebastian.cruz@gmail.com>
2016-11-15 23:01:43 -0800

    Cannon explodes on contact with ship. Works smoothly

--- entry.js
+++ entry.js
@@ -1,7 +1,21 @@
 import Game from './lib/game';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.getElementById('canvas');
+  const canvas = document.getElementById("canvas");
+  canvas.width  = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+
+  function fullscreen(){
+   if(canvas.webkitRequestFullScreen) {
+       canvas.webkitRequestFullScreen();
+   }
+   else {
+     canvas.mozRequestFullScreen();
+  }
+}
+
+  canvas.addEventListener("click",fullscreen);
   const stage = new createjs.Stage(canvas);
   new Game(canvas, stage);
 });
